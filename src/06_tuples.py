48df78179a40995ee065eae68f47547ad1a8d9ea
aaronmatson1 <matsonaaron1@gmail.com>
2020-07-08 22:06:56 -0600

    Updated Tuples

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -36,14 +36,15 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # YOUR CODE HERE
 
+def print_tuple(t):
+    for x in t:
+        print(x)
+
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
-def print_tuple(list):
-    print(type(list))
-    for i in list:
-        print(i)
+
 
 # Declare a tuple of 1 element then print it
 u = (1,)  # What needs to be added to make this work?
-print_tuple(u)
+# print_tuple(u)
